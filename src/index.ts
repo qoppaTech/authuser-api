8105363d5b171db1401032534e260b0019102091
Otávio <otaviorp2014@gmail.com>
2023-10-25 16:15:49 -0300

    Initial structure

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ config();
 
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.listen(() => {
     console.log(`Server running in port ${port}`);
