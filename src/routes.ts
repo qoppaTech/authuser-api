39f8a3d2f95e161dd9a48329d69ae1c9cc446fc5
Marshziin <otaviorp2014@gmail.com>
2023-10-26 19:27:59 -0300

    Correction type of HTTP request

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,7 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router
-.get("/SingUp", UserController.createUser)
+.post("/SingUp", UserController.createUser)
 .get("/LogIn", UserController.LogIn)
 .get("/", (req: Request, res: Response) => res.status(200).send("<p><h1>Welcome!</h1></p> <hr> <h2>Qoppa Tech </h2>"))
 .get("*", (req: Request, res: Response) => res.status(200).send("<h1>ERROR 404 -> PAGE NOT FOUND</h1><hr> <h2>Qoppa Tech </h2>"));
