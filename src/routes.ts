6f9de7e08c79079cb0470a7499d03b72679c073f
Marshziin <otaviorp2014@gmail.com>
2023-10-27 12:59:33 -0300

    Correction in routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -5,8 +5,7 @@ const router = Router();
 
 router
 .post("/SingUp", UserController.createUser)
-.get("/LogIn", UserController.LogIn)
-.get("/", (req: Request, res: Response) => res.status(200).send("<p><h1>Welcome!</h1></p> <hr> <h2>Qoppa Tech </h2>"))
-.get("*", (req: Request, res: Response) => res.status(200).send("<h1>ERROR 404 -> PAGE NOT FOUND</h1><hr> <h2>Qoppa Tech </h2>"));
+.post("/LogIn", UserController.LogIn)
+.get("*", (req: Request, res: Response) => res.status(404).send("<h1>ERROR 404 -> PAGE NOT FOUND</h1><hr> <h2>Qoppa Tech </h2>"));
 
 export default router;
